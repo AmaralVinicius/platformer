1550be79710f5ac00fe6a12584ed4502a9f14114
Vinícius Amaral <amaralvinicius171@gmail.com>
2023-06-05 16:34:28 -0300

    Player sprite
    Refatoração utilizando POO e pygame sprites

--- main.py
+++ main.py
@@ -17,6 +17,114 @@ pygame.display.set_caption('Platformer')
 # Variáveis gerais
 run = True
 
+# Player class
+class Player(pygame.sprite.Sprite):
+    def __init__(self, image, pos):
+        super().__init__()
+        # Variáveis gerais do player
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
+        self.movement = [0, 0]
+        self.vertical_momentum = 0
+        self.air_time = 0
+        # Variáveis de estado do player
+        self.moving_right = False
+        self.moving_left = False
+        self.jumping = False
+        self.jumped = False
+
+    # Retorna toda colisão do player com a lista de tiles passada
+    def collision_test(self, tile_rects):
+        hit_list = []
+
+        for tile in tile_rects:
+            if self.rect.colliderect(tile):
+                hit_list.append(tile)
+
+        return hit_list
+
+    # Movimento e colisões
+    def move(self, tile_rects):
+        self.collisions = {'top': False, 'bottom': False, 'right': False, 'left': False}
+
+         # Movimento e colisões horizontais
+        self.rect.x += self.movement[0]
+        hit_list = self.collision_test(tile_rects)
+        for tile in hit_list:
+            if self.movement[0] > 0:
+                self.rect.right = tile.left
+                self.collisions['right'] = True
+            if self.movement[0] < 0:
+                self.rect.left = tile.right
+                self.collisions['left'] = True
+
+        # Movimento e colisões verticais
+        self.rect.y += self.movement[1]
+        hit_list = self.collision_test(tile_rects)
+        for tile in hit_list:
+            if self.movement[1] > 0:
+                self.rect.bottom = tile.top
+                self.collisions['bottom'] = True
+            if self.movement[1] < 0:
+                self.rect.top = tile.bottom
+                self.collisions['top'] = True
+
+    def update(self, tile_rects):
+        self.movement = [0, 0]
+
+        # Movimento horizontal
+        if self.moving_right:
+            self.movement[0] += 2
+        if self.moving_left:
+            self.movement[0] -= 2
+
+        # Pulo
+        if self.jumping and self.air_time < 5 and not self.jumped:
+            self.jumped = True
+            self.vertical_momentum = -6
+
+        # Movimento vertical (Gravidade)
+        self.vertical_momentum += 0.3
+
+        # Limite de velocidade de queda
+        if self.vertical_momentum >= 4:
+            self.vertical_momentum = 4
+
+        # Aplica o momento vertical ao movimento do player
+        self.movement[1] += self.vertical_momentum
+
+        # Aplica o movimento e colisões
+        self.move(tile_rects)
+
+        # Corrige queda do player caso colida com teto ou chão
+        if self.collisions['top']:
+            self.vertical_momentum = 1
+
+        if self.collisions['bottom']:
+            self.vertical_momentum = 1
+            # Reseta tempo no ar e estado do pulo caso colida com o chão
+            self.air_time = 0
+            self.jumped = False
+        else:
+            # Tempo no ar caso esteja sem colidir com o chão, usado para o pulo e coyote time
+            self.air_time += 1
+
+# Desenha os tiles do mapa e gera os rects para cada tile
+def generate_tiles(tiles_data, dirt_image, grass_image):
+    tile_rects = []
+
+    for row_index, row in enumerate(tiles_data):
+        for tile_index, tile in enumerate(row):
+            if tile == '1':
+                screen.blit(dirt_image, (tile_index * tile_size, row_index * tile_size))
+            if tile == '2':
+                screen.blit(grass_image, (tile_index * tile_size, row_index * tile_size))
+            if tile != '0':
+                tile_rects.append(pygame.Rect(tile_index * tile_size, row_index * tile_size, tile_size, tile_size))
+
+    return tile_rects
+
 # Game Map
 grass_image = pygame.image.load('assets/grass.png').convert()
 dirt_image = pygame.image.load('assets/dirt.png').convert()
@@ -39,64 +147,8 @@ tiles_data = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
 # Player
 player_image = pygame.image.load('assets/player.png').convert()
 player_image.set_colorkey((255, 255, 255))
-player_rect = player_image.get_rect()
-player_rect.topleft = (16, 50)
-player_y_momentum = 0
-player_air_time = 0
-moving_right, moving_left = False, False
-player_collisions = {'top': False, 'bottom': False, 'right': False, 'left': False}
-
-# Retorna lista com todos rects do mapa que colidiu com o player
-def collision_test(player_rect, tile_rects):
-    hit_list = []
-    for tile_rect in tile_rects:
-        if player_rect.colliderect(tile_rect):
-            hit_list.append(tile_rect)
-
-    return hit_list
-
-# Movimento e colisões
-def move(rect, movement, tile_rects):
-    collisions = {'top': False, 'bottom': False, 'right': False, 'left': False}
-
-    # Movimento e colisões horizontais
-    rect.x += movement[0]
-    hit_list = collision_test(rect, tile_rects)
-    for tile in hit_list:
-        if movement[0] > 0:
-            rect.right = tile.left
-            collisions['right'] = True
-        elif movement[0] < 0:
-            rect.left = tile.right
-            collisions['left'] = True
-
-    # Movimento e colisões verticais
-    rect.y += movement[1]
-    hit_list = collision_test(rect, tile_rects)
-    for tile in hit_list:
-        if movement[1] > 0:
-            rect.bottom = tile.top
-            collisions['bottom'] = True
-        elif movement[1] < 0:
-            rect.top = tile.bottom
-            collisions['top'] = True
-
-    return rect, collisions
-
-# Desenha os tiles e gera os rects para cada tile
-def generate_tiles(tiles_data, dirt_image, grass_image):
-    tile_rects = []
-
-    for row_index, row in enumerate(tiles_data):
-        for tile_index, tile in enumerate(row):
-            if tile == '1':
-                screen.blit(dirt_image, (tile_index * tile_size, row_index * tile_size))
-            if tile == '2':
-                screen.blit(grass_image, (tile_index * tile_size, row_index * tile_size))
-            if tile != '0':
-                tile_rects.append(pygame.Rect(tile_index * tile_size, row_index * tile_size, tile_size, tile_size))
-
-    return tile_rects
+player = Player(player_image, (16, 48))
+player_group = pygame.sprite.GroupSingle(player)
 
 # Loop principal
 while run:
@@ -106,17 +158,21 @@ while run:
         if event.type == QUIT:
             run = False
         if event.type == KEYDOWN:
+            # Controle de estados do player
             if event.key == K_RIGHT:
-                moving_right = True
+                player_group.sprite.moving_right = True
             if event.key == K_LEFT:
-                moving_left = True
-            if event.key == K_UP and player_air_time < 6:
-                player_y_momentum = -5
+                player_group.sprite.moving_left = True
+            if event.key == K_UP:
+                player_group.sprite.jumping = True
         if event.type == KEYUP:
+            # Controle de estados do player
             if event.key == K_RIGHT:
-                moving_right = False
+                player_group.sprite.moving_right = False
             if event.key == K_LEFT:
-                moving_left = False
+                player_group.sprite.moving_left = False
+            if event.key == K_UP:
+                player_group.sprite.jumping = False
 
     # Background
     screen.fill((146, 244, 255))
@@ -124,36 +180,9 @@ while run:
     # Gera o mapa
     tile_rects = generate_tiles(tiles_data, dirt_image, grass_image)
 
-    # Movimentação do player
-    player_movement = [0, 0]
-
-    if moving_right:
-        player_movement[0] += 2
-    if moving_left:
-        player_movement[0] -= 2
-
-    # Gravidade do player
-    player_movement[1] += player_y_momentum
-    player_y_momentum += 0.2
-    # Limite da velocidade de queda do player
-    if player_y_momentum >= 3:
-        player_y_momentum = 3
-
-    # Aplicar movimentação e colisões
-    player_rect, player_collisions = move(player_rect, player_movement, tile_rects)
-
-    # Corrige queda do player caso colida com teto ou chão
-    if player_collisions['top']:
-        player_y_momentum = 1
-
-    if player_collisions['bottom']:
-        player_y_momentum = 1
-        player_air_time = 0
-    else:
-        player_air_time += 1
-
-    # Desenha player
-    screen.blit(player_image, player_rect)
+    # Player update e draw
+    player_group.update(tile_rects)
+    player_group.draw(screen)
 
     # Update da tela de jogo
     window.blit(pygame.transform.scale(screen, WINDOW_SIZE), (0, 0))
